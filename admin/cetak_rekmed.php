1409c76a73075df2ae84482576ecbe25f55b202e
Hardiansyah <hardiansyahmail@gmail.com>
2022-11-24 14:18:17 +0700

    update : typo november

--- admin/cetak_rekmed.php
+++ admin/cetak_rekmed.php
@@ -187,7 +187,7 @@ include"../koneksi.php" ?>
   $array_hari = array(1=>'Senin','Selasa','Rabu','Kamis','Jumat','Sabtu','Minggu');
   $hari = $array_hari[date('N')];
 
-  $array_bulan = array(1=>'Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','Novemer','Desember');
+  $array_bulan = array(1=>'Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember');
   $bulan = $array_bulan[date('n')];
 
   $tgl = date('j');
--- admin/laporan_rekam.php
+++ admin/laporan_rekam.php
@@ -132,7 +132,7 @@ $kode = $r['kode_pasien'];
                     $array_hari = array(1=>'Senin','Selasa','Rabu','Kamis','Jumat','Sabtu','Minggu');
                     $hari = $array_hari[date('N')];
 
-                    $array_bulan = array(1=>'Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','Novemer','Desember');
+                    $array_bulan = array(1=>'Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember');
                     $bulan = $array_bulan[date('n')];
 
                     $tgl = date('j');
